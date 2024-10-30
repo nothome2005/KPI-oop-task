1f8aa7367add9d2505df82f29c2ceba3cc423406
Dmutro <dimonshevchuk1@gmail.com>
2024-10-30 09:31:30 +0200

    Update lab1_unit(lab2_test).cpp

--- lab1_unit/lab1_unit.cpp
+++ lab1_unit/lab1_unit.cpp
@@ -9,6 +9,11 @@ int main()
 {
     Unit warrior("warrior", 100, 20, 5, 0, 0, 2);
     Unit archer("archer", 80, 15, 3, 1, 1, 3);
+    //lab2_backpack_test
+    Item sword("Sword", 15);
+    warrior.addItemToBackpack(sword);
+    cout<<"Warrior's backpack:"<<endl;
+    warrior.showBackpack();
 
     warrior.move(1, 1);
     cout << "warrior moved to new position!" << endl;
