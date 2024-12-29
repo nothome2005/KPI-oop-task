6b5c9f2b4b258b06dcff48bdeb6b82963f010865
Dmutro <dimonshevchuk1@gmail.com>
2024-12-29 21:44:50 +0200

    Update Unit.h(lab3)

--- lab1_unit/Unit.h
+++ lab1_unit/Unit.h
@@ -16,16 +16,45 @@ private:
 Backpack backpack;
 
 public:
-	Unit(string n, int hp, int ap, int def, int x, int y, int spd)
-		: name(n), health(hp), attackPower(ap), defense(def), posX(x), posY(y), speed(spd), backpack(maxBackpackWeight) {}
+    Unit(string n, int hp, int ap, int def, int x, int y, int spd, int maxBackpackWeight)
+        : name(n), health(hp), attackPower(ap), defense(def), posX(x), posY(y), speed(spd), backpack(maxBackpackWeight) {}
 
-	void move(int x, int y);
-	void attack(Unit& target);
-	void takeDamage(int damage);
-	bool isAlive() const;
-	int getHealth() const;
+    virtual ~Unit() = default;
 
-	bool addItemToBackpack(const Item& item);
-	void showBackpack() const;
+
+   virtual void move(int x, int y);
+    virtual void attack(Unit& target);
+    virtual void takeDamage(int damage);
+    virtual void specialAbility() = 0;
+
+    bool isAlive() const;
+    int getHealth() const;
+    string getName() const { return name; }
+    bool addItemToBackpack(const Item& item);
+    void showBackpack() const;
+
+    int getX() const { return posX; } 
+    int getY() const { return posY; } 
+};
+class Warrior : public Unit {
+public:
+    Warrior(int x, int y)
+        : Unit("Warrior", 120, 25, 10, x, y, 2, 100) {}  
+
+    void specialAbility() override {
+        cout << "Warrior uses Shield Block to reduce incoming damage!" << endl;
+        defense += 5;
+    }
+};
+
+class Archer : public Unit {
+public:
+    Archer(int x, int y)
+        : Unit("Archer", 90, 20, 5, x, y, 3, 80) {}  
+
+    void specialAbility() override {
+        cout << "Archer shoots a powerful arrow!" << endl;
+        attackPower += 10;
+    }
 };
 
