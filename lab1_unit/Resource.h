9efacec99c675438b0a3a66a33e1a17c023c530e
Dmutro <dimonshevchuk1@gmail.com>
2024-12-30 20:28:34 +0200

    Update Resource.h(lab4)

--- lab1_unit/Resource.h
+++ lab1_unit/Resource.h
@@ -3,6 +3,39 @@
 
 using namespace std;
 
+#include <vector>
+
+// Інтерфейс спостерігача
+class Observer {
+public:
+    virtual void update(const Resource& resource) = 0;
+    virtual ~Observer() = default;
+};
+
+// Суб'єкт (ресурс)
+class Resource {
+    vector<Observer*> observers;
+    int amount;
+public:
+    void attach(Observer* observer) {
+        observers.push_back(observer);
+    }
+    void detach(Observer* observer) {
+        observers.erase(remove(observers.begin(), observers.end(), observer), observers.end());
+    }
+    void notify() {
+        for (auto observer : observers) {
+            observer->update(*this);
+        }
+    }
+    void collect(int qty) {
+        if (qty <= amount) {
+            amount -= qty;
+            notify();
+        }
+    }
+};
+
 class Resource {
 private:
     string name;
